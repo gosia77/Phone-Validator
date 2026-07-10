b419dc93a20df0a92255ba0441a81de009fcca33
PCGosia <malgorzataserafinlatka@gmail.com>
2026-07-10 09:07:53 +0200

    Add click outside. Clean code.

--- phone-validator/src/App.vue
+++ phone-validator/src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from "vue";
 const PhoneValidator = defineAsyncComponent(() => import("./components/PhoneValidator.vue"));
-const PhoneExample = defineAsyncComponent(() => import("./components/PhoneExample.vue"));
 import './index.css'
 
 </script>
 
 <template>
     <PhoneValidator />
-    <PhoneExample />
 </template>
